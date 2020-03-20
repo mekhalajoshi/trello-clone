2f61e3e4332926d3557fb6d3beb7ce8890d6c77f
Mekhala Joshi <mekhalajoshi@gmail.com>
2020-03-20 11:44:44 -0400

    Local dev env setup

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <div>Welcome to my-webpack-react-starter!!!!!!!</div>
+  </div>
+);
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable no-undef */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
