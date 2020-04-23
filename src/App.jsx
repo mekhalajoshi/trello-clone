2c7e9150ece8092e699ca7af94a8753d14417ed4
Mekhala Joshi <mekhalajoshi@users.noreply.github.com>
2020-04-23 13:24:26 -0400

    Navbar (#3)

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import { Provider as ReduxProvider } from 'react-redux';
 import Board from './components/trello/Board';
 import Navbar from './components/Navbar';
 import Register from './components/auth/Register';
-import Welcome from './components/auth/Welcome';
+import SignUpMessage from './components/auth/SignUpMessage';
+import LogOutMessage from './components/auth/LogOutMessage';
 import ChangePassword from './components/auth/ChangePassword';
 import ChangePasswordConfirm from './components/auth/ChangePasswordConfirm';
 import ForgotPassword from './components/auth/ForgotPassword';
@@ -13,6 +14,7 @@ import ForgotPasswordVerification from './components/auth/ForgotPasswordVerifica
 import configureStore from './redux/store/configureStore';
 import LogIn from './components/auth/LogIn';
 import initialData from './initial_data';
+import Home from './components/Home';
 
 const store = configureStore(initialData);
 
@@ -23,14 +25,16 @@ const App = () => (
         <div>
           <Navbar />
           <Switch>
-            <Route exact path="/" component={Board} />
+            <Route exact path="/" component={Home} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={LogIn} />
-            <Route exact path="/welcome" component={Welcome} />
+            <Route exact path="/logout" component={LogOutMessage} />
+            <Route exact path="/welcome" component={SignUpMessage} />
             <Route exact path="/forgotpassword" component={ForgotPassword} />
             <Route exact path="/forgotpasswordverification" component={ForgotPasswordVerification} />
             <Route exact path="/changepassword" component={ChangePassword} />
             <Route exact path="/changepasswordconfirmation" component={ChangePasswordConfirm} />
+            <Route exact path="/board" component={Board} />
           </Switch>
         </div>
       </Router>
---
+++ src/components/auth/LogOutMessage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function LogOutMessage() {
+  return (
+    <section className="section auth">
+      <div className="container">
+        <p>You have successfully Logged Out.</p>
+      </div>
+    </section>
+  );
+}
